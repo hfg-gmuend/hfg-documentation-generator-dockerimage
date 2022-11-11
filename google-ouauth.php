08ed8c5986098afb64a172b058e7c6ed72b0c12e
Florian Geiselhart <19553942+fg-uulm@users.noreply.github.com>
2022-11-11 13:48:45 +0100

    Switching to getenv instead $_ENV

--- google-ouauth.php
+++ google-ouauth.php
@@ -1,4 +1,4 @@
 <?php
 
-c::set('client_id', $_ENV["OAUTH_CLIENT_ID"]);
-c::set('redirect_uri', $_ENV["OAUTH_REDIRECT_URI"]);
+c::set('client_id', getenv('OAUTH_CLIENT_ID'));
+c::set('redirect_uri', getenv('OAUTH_REDIRECT_URI'));
